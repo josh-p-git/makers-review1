407aba0911cf7adec8aa39ffd667b9f6d82db572
josh-p-git <joshua.penney91@gmail.com>
2023-02-23 16:00:12 +0000

    end of test

--- spec/report.test.js
+++ spec/report.test.js
@@ -7,3 +7,12 @@ describe('Report can be initialised', () => {
         expect(report.results).toEqual('Green, Green, Amber, Red, Green');
     });
 });
+
+describe('Results can be filtered', () => {
+    const report = new Report("Green, Green, Green");
+
+    test('A result of Green can be filtered', () => {
+        expect(report.filterResults()).toContain("Green: 3");
+    });
+
+});
